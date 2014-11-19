6051419a0750411b989a0d5f922a09edd11c3a74
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2014-11-19 23:23:02 +0100

    Weird bug in regards to the DRY principle of initiating Headroom for both header as footer

--- src/wp-content/themes/twentyfifteen-child/js/scripts.js
+++ src/wp-content/themes/twentyfifteen-child/js/scripts.js
@@ -1,10 +1,19 @@
 
 // Intitalize Headroom.js to various elements
-var elements = [ '#header', '#footer' ];
-var i = elements.length;
-while( --i ){
+// var elements = [ '#header', '#footer' ];
+// var i = elements.length;
+// while( --i ){
 
-	var element = document.querySelector( elements[ i ]);
-	var headroom = new Headroom( element );
-	headroom.init();
-}
+// 	var element = document.querySelector( elements[ i ]);
+// 	var headroom = new Headroom( element );
+// 	headroom.init();
+// }
+
+
+var header = document.querySelector( '#header' );
+var headerHeadroom = new Headroom( header );
+headerHeadroom.init();
+
+var footer = document.querySelector( '#footer' );
+var footerHeadroom = new Headroom( footer );
+footerHeadroom.init();
