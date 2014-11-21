411881101f735c11864a36d843636912f797ee2a
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2014-11-21 13:04:09 +0100

    Added _hentry.less

---
+++ src/wp-content/themes/twentyfifteen-child/content-page.php
@@ -0,0 +1,25 @@
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+	<?php twentyfifteen_post_thumbnail(); ?>
+
+	<header class="entry-header">
+		<?php // the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+	</header><!-- .entry-header -->
+
+	<div class="entry-content">
+		<div class="container">
+			<?php the_content(); ?>
+		</div>
+
+		<?php
+			// wp_link_pages( array(
+			// 	'before'      => '<div class="page-links"><span class="page-links-title">' . esc_html__( 'Pages:', 'twentyfifteen' ) . '</span>',
+			// 	'after'       => '</div>',
+			// 	'link_before' => '<span>',
+			// 	'link_after'  => '</span>',
+			// ) );
+		?>
+	</div><!-- .entry-content -->
+
+	<?php edit_post_link( esc_html__( 'Edit', 'twentyfifteen' ), '<footer class="entry-footer"><div class="container"><div class="row"><div class="col-md-10"><span class="edit-link">', '</span></div></div></div></footer><!-- .entry-footer -->' ); ?>
+
+</article><!-- #post-## -->
