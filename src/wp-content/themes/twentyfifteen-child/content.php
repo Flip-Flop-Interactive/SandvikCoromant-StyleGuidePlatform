305bb2454852ba559fe4e722f295bcea6713f0ed
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2014-11-21 11:43:07 +0100

    Some temporary changes for presentation purposes...

--- src/wp-content/themes/twentyfifteen-child/content.php
+++ src/wp-content/themes/twentyfifteen-child/content.php
@@ -1,14 +1,3 @@
-<?php
-/**
- * The default template for displaying content.
- *
- * Used for both single and index/archive/search.
- *
- * @package WordPress
- * @subpackage Twenty_Fifteen
- * @since Twenty Fifteen 1.0
- */
-?>
 <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfifteen_post_thumbnail(); ?>
 
@@ -48,7 +37,14 @@
 	?>
 
 	<footer class="entry-footer">
-		<?php twentyfifteen_entry_meta(); ?>
-		<?php edit_post_link( esc_html__( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+		<div class="container">
+			<div class="row">
+				<div class="col-md-10">
+					<?php twentyfifteen_entry_meta(); ?>
+					<?php edit_post_link( esc_html__( 'Edit', 'twentyfifteen' ), '<span class="edit-link">', '</span>' ); ?>
+				</div>
+			</div>
+		</div>
 	</footer><!-- .entry-footer -->
+	
 </article><!-- #post-## -->
