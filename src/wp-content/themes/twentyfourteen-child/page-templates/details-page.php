e8dcd17570ae9ba02c82b9f43d3ce125652737e5
Koen Dirk van Esterik <koen@flipflopinteractive.com>
2015-01-09 13:39:52 +0100

    Some iPad adjustments...

--- src/wp-content/themes/twentyfourteen-child/page-templates/details-page.php
+++ src/wp-content/themes/twentyfourteen-child/page-templates/details-page.php
@@ -20,7 +20,7 @@
 				<div class="entry-header">
 					<div class="container">
 						<div class="row">
-							<div class="col-md-10"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></div>
+							<div class="col-lg-10 col-md-10 col-sm-10 col-xs-10"><?php the_title( '<h1 class="entry-title">', '</h1>' ); ?></div>
 						</div>
 					</div>
 				</div>
@@ -31,9 +31,9 @@
 					<div class="container">
 						<hr/>
 						<div class="row">
-							<div class="col-md-4"><h1 class="entry-title"><?php echo get_post( $post->post_parent )->post_title; ?></h1></div>
-							<div class="col-md-4"><?php echo wpautop( $post->post_content ); ?></div>
-							<div class="col-md-2"><?php echo render_download_link( $post->ID ); ?></div>
+							<div class="col-lg-4 col-md-10 col-sm-10 col-xs-10"><h1 class="entry-title"><?php echo get_post( $post->post_parent )->post_title; ?></h1></div>
+							<div class="col-lg-4 col-md-8 col-sm-8 col-xs-10"><?php echo wpautop( $post->post_content ); ?></div>
+							<div class="col-lg-2 col-md-2 col-sm-2 col-xs-10"><?php echo render_download_link( $post->ID ); ?></div>
 						</div>
 						<?php echo render_images( $post->ID ); ?>
 					</div>
